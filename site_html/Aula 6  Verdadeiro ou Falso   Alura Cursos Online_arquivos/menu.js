e8b408741539c0c284ffa2ec8df22d1700735fb3
Gustavo Dias <gustavodias2@gmail.com>
2020-04-19 00:47:33 -0300

    Removendo site html

--- site_html/Aula 6  Verdadeiro ou Falso   Alura Cursos Online_arquivos/menu.js
+++
@@ -1,1 +0,0 @@
-(function(doc){var titulo=doc.querySelector('.header-menu-titulo');var menu=doc.querySelector('.header-menu');if(titulo){titulo.onclick=function(){if(menu.hasAttribute('data-ativo')){menu.removeAttribute('data-ativo')}else{menu.setAttribute('data-ativo','')}}}})(document)
